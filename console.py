b756239098fb84d19f3581e3254d9fb6e1744df6
Gaby-Do <eme.gaby.do@gmail.com>
2022-04-10 09:00:45 -0700

    updating help_create

--- console.py
+++ console.py
@@ -138,7 +138,7 @@ class HBNBCommand(cmd.Cmd):
     def help_create(self):
         """ Help information for the create method """
         print("Creates a class of any type")
-        print("[Usage]: create <className>\n")
+        print("[Usage]: create <Class name> <param 1> <param 2> <param 3>...\n")
 
     def do_show(self, args):
         """ Method to show an individual object """
