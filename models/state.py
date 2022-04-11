43cd3ec3bf56316f88800ded91a7b41494a65536
Gaby-Do <eme.gaby.do@gmail.com>
2022-04-11 15:27:08 -0700

    state

--- models/state.py
+++ models/state.py
@@ -10,3 +10,16 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="states", cascade="all, delete-orphan")
+
+    @property
+    def cities(self):
+        """
+        returns the list of City instances with state_id
+        equals to the current State.id
+        """
+        city_list = []
+        all_cities = storage.all(City)
+        for one_city in all_cities:
+            if state_id is self.id:
+                city_list.append(one_city)
+        return city_list
